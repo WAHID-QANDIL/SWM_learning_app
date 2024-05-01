87f16545f76440c446388700811348d8570c08ee
Wahid Ali <1213089@dtu.edu.eg>
2024-05-01 07:59:26 +0300

    add register fragment and listeners for buttons and text views

--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/MainActivity.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/MainActivity.java
@@ -1,24 +1,105 @@
 package com.dtu.swm_learningapp;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        initSplashScreen();
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        setLoginOnClicks();
+
     }
+
+    private void setLoginOnClicks()
+    {
+        TextView registerTextView = findViewById(R.id.login_register_textview);
+        ImageView nav_register_imageview = findViewById(R.id.nav_register_imageview);
+        registerTextView.setOnClickListener(this);
+        nav_register_imageview.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (id == R.id.login_register_textview || id == R.id.nav_register_imageview)
+        {
+            RegisterFragment registerFragment = new RegisterFragment();
+                fragmentManager.beginTransaction()
+                        .setCustomAnimations(R.anim.slide_in_right,R.anim.slide_in_left)
+                        .add(R.id.login_frame_container, registerFragment)
+                        .addToBackStack(null)
+                        .commit();
+            return;
+        } else if (id == R.id.nav_button || id == R.id.already_have_account_textview) {
+
+            fragmentManager.popBackStack();
+//            fragmentManager.beginTransaction()
+//                    .replace(R.id.login_frame_container,new LoginFragment())
+//                    .setCustomAnimations(R.anim.slide_in_left,R.anim.slide_in_right)
+//                    .addToBackStack(null)
+//                    .commit();
+        }
+        //
+
+    }
+
+
+    private void initSplashScreen()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+        {
+            SplashScreen.installSplashScreen(MainActivity.this);
+            getSplashScreen().setOnExitAnimationListener(splashScreenView -> {
+                final ObjectAnimator slideUp = ObjectAnimator.ofFloat(
+                        splashScreenView,
+                        View.TRANSLATION_X,
+                        (float) -splashScreenView.getWidth() /4,
+                        splashScreenView.getWidth()
+                );
+                slideUp.setInterpolator(new AnticipateInterpolator());
+                slideUp.setDuration(500L);
+
+                slideUp.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        splashScreenView.remove();
+                    }
+                });
+
+                slideUp.start();
+            });
+
+
+
+
+        }
+        else {
+            setTheme(R.style.AppTheme);
+        }
+
+    }
+
+
 }
