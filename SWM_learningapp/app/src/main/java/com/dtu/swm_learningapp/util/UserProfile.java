71aba31098c3e6094532ae44a15ed352e0fa8850
roshdyosf <yosef2236303@gmail.com>
2024-05-10 03:58:13 +0300

    improving code efficiency making code more cleaner adding comments

---
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/util/UserProfile.java
@@ -0,0 +1,36 @@
+package com.dtu.swm_learningapp.util;
+
+import android.content.Context;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserProfile {
+    FirebaseFirestore fStore ;
+
+    public UserProfile() {
+        fStore = FirebaseFirestore.getInstance();
+    }
+
+    public Map<String, Object> preparingData(String name, String email, String pass, String mobNum){
+        //making map container to prepare the data that user entered
+        Map<String, Object>user=new HashMap<>();
+        user.put("name",name);
+        user.put("email",email);
+        user.put("pass",pass);
+        user.put("mobile number",mobNum);
+        return user;
+    }
+    public void savingData(Context context, Map<String, Object>user,String userId){
+        //saving the user info in fire store using user id
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.set(user).addOnSuccessListener(unused -> {
+
+        }).addOnFailureListener(e -> {
+
+        });
+    }
+}
