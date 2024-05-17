ec93e3ed520a70396148ede27aa702fddfcdb01c
Wahid Ali <1213089@dtu.edu.eg>
2024-05-17 22:57:31 +0300

    Add network tracking for check the internet connection

---
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/util/NetworkUtil.java
@@ -0,0 +1,19 @@
+package com.dtu.swm_learningapp.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+
+    public static boolean isNetworkConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (cm != null) {
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+            }
+        }
+        return false;
+    }
+}
