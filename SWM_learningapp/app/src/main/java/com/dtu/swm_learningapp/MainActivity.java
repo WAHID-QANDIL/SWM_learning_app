3c0c6897eba7add717b37b753f6b05e20e5fae35
Wahid Ali <1213089@dtu.edu.eg>
2024-05-17 23:02:34 +0300

    Add network checker to detect the network connection

--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/MainActivity.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnticipateInterpolator;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+
+import com.dtu.swm_learningapp.util.NetworkUtil;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
     //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        if (!NetworkUtil.isNetworkConnected(this)) {
+            // Network is connected
+            Snackbar snackbar = Snackbar.make(
+                    findViewById(R.id.main_activity),
+                    R.string.no_internet_connection,
+                    Snackbar.LENGTH_SHORT).setTextColor(getResources().getColor(R.color.colorAccent,this.getTheme()));
+            snackbar.setAction("Ok", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+            });
+            snackbar.show();
+        }
     }
     private void initSplashScreen()
     {
