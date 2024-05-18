1c00aab86ce476a21f45e8848efec5fd7e533007
Wahid Ali <1213089@dtu.edu.eg>
2024-05-18 12:24:46 +0300

    forget password fix

--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/ForgetPasswordFragment.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/ForgetPasswordFragment.java
@@ -2,71 +2,68 @@ package com.dtu.swm_learningapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.dtu.swm_learningapp.databinding.FragmentForgetPasswordBinding;
+import com.dtu.swm_learningapp.util.ToastMaker;
+import com.dtu.swm_learningapp.util.Validation;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForgetPasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ForgetPasswordFragment extends BottomSheetDialogFragment {
+import java.util.Objects;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ForgetPasswordFragment extends BottomSheetDialogFragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ForgetPasswordFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForgetPasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForgetPasswordFragment newInstance(String param1, String param2) {
-        ForgetPasswordFragment fragment = new ForgetPasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    static FirebaseAuth fAuth;
+    private FragmentForgetPasswordBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        fAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View buttomSheet = inflater.inflate(R.layout.fragment_forget_password, container, false);
 
-        buttomSheet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                dismiss();
-            }
-        });
-        return buttomSheet;
+        binding = FragmentForgetPasswordBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btSend.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == binding.btSend.getId() && Validation.emailIsValid(Objects.requireNonNull(binding.etEmail.getText()).toString())) {
+            String email = binding.etEmail.getText().toString();
+            fAuth.sendPasswordResetEmail(email).addOnSuccessListener(unused ->{
+                                    dismiss();
+                                    Snackbar.make(requireView(),
+                                    "An email was send to "+ binding.etEmail.getText().toString(),
+                                    Snackbar.LENGTH_LONG).show();
+            }).
+            addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                            dismiss();
+                            Snackbar.make(requireView(),
+                            "Can't get email like this "+ binding.etEmail.getText().toString(),
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
