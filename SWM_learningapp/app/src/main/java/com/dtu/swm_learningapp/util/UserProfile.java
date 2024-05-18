73beedfda1997ca0021bc293a3a5648c5cfd4248
Wahid Ali <1213089@dtu.edu.eg>
2024-05-18 12:25:32 +0300

    code begging

--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/util/UserProfile.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/util/UserProfile.java
@@ -1,6 +1,7 @@
 package com.dtu.swm_learningapp.util;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -10,22 +11,23 @@ import java.util.Map;
 
 public class UserProfile {
     FirebaseFirestore fStore ;
+    private static final String TAG = "UserProfile";
 
     public UserProfile() {
         fStore = FirebaseFirestore.getInstance();
     }
 
-    public Map<String, Object> getUserProfile(String name, String email, String pass, String mobNum){
+    public Map<String, Object> getUserProfile(String name, String email, String mobNum){
         //making map container to prepare the data that user entered
         Map<String, Object>user=new HashMap<>();
         user.put("name",name);
         user.put("email",email);
-        user.put("pass",pass);
         user.put("mobile number",mobNum);
         return user;
     }
     public void savingData(Context context, Map<String, Object>user,String userId){
         //saving the user info in fire store using user id
+        Log.d(TAG, "savingData: ");
         DocumentReference documentReference = fStore.collection("users").document(userId);
         documentReference.set(user).addOnSuccessListener(unused -> {
 
