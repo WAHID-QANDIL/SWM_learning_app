8c2a52aa2bbbf6e949cf8e588adbc3e1790c2422
Wahid Ali <1213089@dtu.edu.eg>
2024-05-18 12:20:39 +0300

    add init listeners and handle logout button clicks

--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/HomeFragment.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/HomeFragment.java
@@ -1,20 +1,25 @@
 package com.dtu.swm_learningapp;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.dtu.swm_learningapp.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
     private FragmentHomeBinding binding;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
 
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -28,11 +33,37 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 //        LoginFragment.fAuth.signOut(); // to test logic for now
+        initListeners();
    }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void initListeners()
+    {
+        binding.logoutButton.setOnClickListener(this);
+        binding.HomeCard.setOnClickListener(this);
+        binding.notesCard.setOnClickListener(this);
+        binding.settingsCard.setOnClickListener(this);
+        binding.promodoroCard.setOnClickListener(this);
+    }
+//hosam@gmail.com : email
+//123456789 : pass
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if  (id == binding.logoutButton.getId())
+        {
+            LoginFragment.fAuth.signOut();
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment_to_loginFragment);
+        }
+        if (id == binding.HomeCard.getId())
+        {
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment_to_user_profile);
+        }
     }
 }
