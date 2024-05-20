91926576898b0c9ae033c21d6912b98d11a96b44
Wahid Ali <1213089@dtu.edu.eg>
2024-05-21 01:20:32 +0300

    Add live data to fetch user data from firestore

--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/HomeUserFragment.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/HomeUserFragment.java
@@ -1,64 +1,141 @@
 package com.dtu.swm_learningapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeUserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeUserFragment extends Fragment {
+import com.dtu.swm_learningapp.databinding.FragmentHomeUserBinding;
+import com.dtu.swm_learningapp.model.User;
+import com.dtu.swm_learningapp.ui.HomeUserVM;
+import com.dtu.swm_learningapp.util.ResetUserInfo;
+import com.dtu.swm_learningapp.util.ToastMaker;
+import com.dtu.swm_learningapp.util.UserDataFetcher;
+import com.dtu.swm_learningapp.util.Validation;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.Objects;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeUserFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeUserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeUserFragment newInstance(String param1, String param2) {
-        HomeUserFragment fragment = new HomeUserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class HomeUserFragment extends Fragment implements View.OnClickListener {
+    private FragmentHomeUserBinding binding;
+    ResetUserInfo resetUserInfo;
+    FirebaseAuth firebaseAuth;
+    private HomeUserVM homeUserVM;
+    User user;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // init view model
+        homeUserVM = new ViewModelProvider(this).get(HomeUserVM.class);
+        firebaseAuth = FirebaseAuth.getInstance();
+        resetUserInfo = new ResetUserInfo(firebaseAuth.getUid());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_user, container, false);
+        binding = FragmentHomeUserBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btSave.setOnClickListener(this);
+        binding.btResetPass.setOnClickListener(this);
+        homeUserVM.requestUserData(requireContext());
+        observeUSerData();
+    }
+
+    private void observeUSerData() {
+        homeUserVM.getUserData().observe(getViewLifecycleOwner(), _user -> {
+            user = _user;
+            setDataToFields();
+        });
+
+    }
+
+    private void setDataToFields() {
+        binding.etName.setText(user.getName());
+        binding.etEmail.setText(user.getEmail());
+        binding.etPhone.setText(user.getPhone());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == binding.btSave.getId()) {
+
+            if (isValid(binding.etName.getText().toString(),
+                    binding.etEmail.getText().toString(),
+                    binding.etPhone.getText().toString(),
+                    binding.etOldPass.getText().toString())) {
+
+                resetUserInfo.updateNameInFireStore(getContext(),
+                        binding.etName.getText().toString(), user.getName());
+
+                resetUserInfo.updatePhoneInFireStore(getContext(),
+                        binding.etPhone.getText().toString(), user.getPhone());
+
+                resetUserInfo.reAuthenticateAndUpdateEmail(requireContext(), Objects.requireNonNull(firebaseAuth.getCurrentUser()),
+                        binding.etOldPass.getText().toString(),
+                        binding.etEmail.getText().toString());
+
+            }
+        }
+        if (v.getId() == binding.btResetPass.getId() &&
+                isPassValid(binding.etOldPass.getText().toString(),
+                        binding.etNewPass.getText().toString())) {
+            ToastMaker.toastShower(requireContext(), "here");
+            resetUserInfo.reAuthenticateAndChangePassword(requireContext(), Objects.requireNonNull(firebaseAuth.getCurrentUser()),
+                    binding.etOldPass.getText().toString(),
+                    binding.etNewPass.getText().toString());
+        }
+    }
+
+
+    public boolean isValid(String name, String email, String phone, String pass) {
+        if (!Validation.nameIsValid(name)) {
+            //showing error massage if the name isn`t valid
+            binding.etName.setError("name is not valid");
+            return false;
+        }
+
+        if (!Validation.emailIsValid(email)) {
+            //showing error massage if the email isn`t valid
+            binding.etEmail.setError("email is not correctly formatted");
+            return false;
+        }
+        if (!Validation.phoneIsValid(phone)) {
+            //showing error massage if the mobile number isn`t valid
+
+            binding.etPhone.setError("mobile number is not valid");
+            return false;
+        }
+        if (!Validation.passIsValid(pass)) {
+            binding.etOldPass.setError("please enter a valid password");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isPassValid(String oldPass, String newPass) {
+        if (!Validation.passIsValid(newPass) && !Validation.passIsValid(oldPass)) {
+            binding.etOldPass.setError("please enter a valid password");
+            return false;
+        }
+        return true;
     }
 }
--- SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/util/ResetUserInfo.java
+++ SWM_learningapp/app/src/main/java/com/dtu/swm_learningapp/util/ResetUserInfo.java
@@ -10,16 +10,16 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ResetUserInfo {
     static FirebaseAuth fAuth = FirebaseAuth.getInstance();
-    FirebaseFirestore db;
-    DocumentReference userRef;
+    private final DocumentReference userRef;
 
     public ResetUserInfo(String userId) {
-        db = FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         userRef = db.collection("users").document(userId);
     }
 
-    public void updateNameInFireStore(Context context, String newName) {
+    public void updateNameInFireStore(Context context, String newName ,String oldName ) {
         if (Validation.nameIsValid(newName)) {
+            if(newName.equals(oldName)) return;
             userRef.update("name", newName)
                     .addOnSuccessListener(unused -> {
                         ToastMaker.toastShower(context, "Name updated successfully.");
@@ -32,8 +32,9 @@ public class ResetUserInfo {
         }
     }
 
-    public void updatePhoneInFireStore(Context context, String newPhone) {
+    public void updatePhoneInFireStore(Context context, String newPhone,String oldPhone) {
         if (Validation.phoneIsValid(newPhone)) {
+            if(newPhone.equals(oldPhone))return;
             userRef.update("mobile number", newPhone)
                     .addOnSuccessListener(unused -> {
                         ToastMaker.toastShower(context, "Phone number updated successfully.");
@@ -45,9 +46,44 @@ public class ResetUserInfo {
             ToastMaker.toastShower(context, "Invalid phone number format.");
         }
     }
+    public void reAuthenticateAndUpdateEmail(Context context, FirebaseUser user, String oldPassword, String newEmail) {
+        String email = user.getEmail();
+        if (email == null) {
+            ToastMaker.toastShower(context, "User email not found.");
+            return;
+        }
+        if (email.equals(newEmail)) {
+            return;
+        }
+        //update email in fireBase
+        AuthCredential credential = EmailAuthProvider.getCredential(email, oldPassword);
 
+        user.reauthenticate(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        user.verifyBeforeUpdateEmail(newEmail)
+                                .addOnCompleteListener(updateTask -> {
+                                    if (updateTask.isSuccessful()) {
+                                        // Update email in FireStore
+                                        userRef.update("email", newEmail)
+                                                .addOnSuccessListener(unused -> {
+                                                    ToastMaker.toastShower(context, "Email updated successfully.");
+                                                })
+                                                .addOnFailureListener(e -> {
+                                                    ToastMaker.toastShower(context, "Failed to update email in Firestore.");
+                                                });
+                                    } else {
+                                        ToastMaker.toastShower(context, "Failed to update email.");
+                                    }
+                                });
+                    } else {
+                        ToastMaker.toastShower(context, "Reauthentication failed.");
+                    }
+                });
+    }
     public void reAuthenticateAndChangePassword(Context context, FirebaseUser user, String oldPassword, String newPassword) {
         String email = user.getEmail();
+
         if (email == null) {
             ToastMaker.toastShower(context, "User email not found.");
             return;
